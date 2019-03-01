f8e4e1ccfb07237062283575baf1effd478364d3
mrsalvini <dsgmcosta@gmail.com>
2019-03-01 16:59:56 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  # arr.sort_by { |word|
+  #   word.each_char { |c|
+  #     esperanto_alphabet.index(c)
+  #   }
+  # }
+
+  arr.sort_by { |word|
+    word.split(%r{\s*}).map { |c|
+      esperanto_alphabet.index(c)
+    }
+  }
+
 end
